a1081b2d8308e1b4ac091402c56a3a6f2b9514dd
Jonathan Soto <37603928+JonathanDSoto@users.noreply.github.com>
2024-10-28 22:47:06 -0700

    ultimos cambios

--- home.php
+++ home.php
@@ -1,12 +1,16 @@
 <?php 	
 	include_once "app/ProductController.php";
+	include_once "app/BrandController.php";
+
 	$productController = new ProducController();
+	$brandController = new BrandController();
 
 
 	if (isset($_SESSION['user_id']) && $_SESSION['user_id']!=null) {
 		
 
 		$products = $productController->get();
+		$brands = $brandController->get();
 
 
 	}else{
@@ -136,12 +140,35 @@
 
 
 					<div id="main">
+
+						<div class="container ">
+						  <nav aria-label="breadcrumb">
+						    <ol class="breadcrumb p-3 bg-body-tertiary rounded-3">
+						      
+						      <li class="breadcrumb-item active">
+						      	<a href="#">
+						      		Productos
+						      	</a>
+						      </li>
+						       <li class="float-end ">
+						       	
+						       		<button data-bs-toggle="modal" data-bs-target="#exampleModal" type="button" class="btn btn-primary">
+						       			Add producto
+						       		</button>
+
+						       </li>
+						    </ol>
+						  </nav>
+
+
+
+						</div>
 						
 						<div class="container p-3"> 
 
-							<h2>
+							<!-- <h2>
 								Lista de productso disponibles
-							</h2>
+							</h2> -->
 
 							<div class="row"> 
 
@@ -164,6 +191,16 @@
 								    <a href="details.php?slug=<?= $product->slug  ?>" class="btn btn-primary">
 								    	Ver detalles
 								    </a>
+
+								    <button onclick='editar(this)' data-product='<?= json_encode($product)  ?>' data-bs-toggle="modal" data-bs-target="#updateModal" type="button" class="btn btn-warning">
+								    	Editar
+								    </button>
+
+								     
+								    <button onclick="eliminar(<?= $product->id ?>)" type="button" class="btn btn-danger m-2">
+								    	Eliminar
+								    </button>
+
 								  </div>
 								</div>
 
@@ -183,11 +220,218 @@
 	 	</div>
 		
 
-		
+			
 
 
 	</div>
+
+	<!-- Modal -->
+	<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	  <div class="modal-dialog">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <h1 class="modal-title fs-5" id="exampleModalLabel">
+	        	Añadir producto
+	        </h1>
+	        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+	      </div>
+	      <div class="modal-body">
+	        <form method="POST" action="app/ProductController.php">
+			  
+			  <div class="mb-3">
+			    <label for="nombre" class="form-label">
+			    	Nombre
+			    </label>
+			    <input type="text" class="form-control" id="nombre" name="nombre" aria-describedby="emailHelp" required> 
+			  </div>
+			  
+			  <div class="mb-3">
+			    <label for="slug" class="form-label">
+			    	Slug
+			    </label>
+			    <input type="text" class="form-control" id="slug" name="slug" required> 
+			  </div>
+
+			  <div class="mb-3">
+			    <label for="slug" class="form-label">
+			    	description
+			    </label>
+			    <input type="text" class="form-control" id="description" name="description" required> 
+			  </div>
+
+			  <div class="mb-3">
+			    <label for="slug" class="form-label">
+			    	features
+			    </label>
+			    <input type="text" class="form-control" id="features" name="features" required> 
+			  </div> 
+
+			  <div class="mb-3">
+			    <label for="slug" class="form-label">
+			    	Marca
+			    </label>
+			    
+			    <select class="form-control">
+			    	<?php if (isset($brands) && count($brands)): ?>
+			    	<?php foreach ($brands as $brand): ?>
+			    	<option value="<?= $brand->id ?>">
+			    		<?= $brand->name ?>
+			    	</option>
+			    	<?php endforeach ?>
+			    	<?php endif ?>
+			    	
+			    </select>
+
+			  </div>
+			  
+			  <button type="submit" class="btn btn-primary">
+			  	Crear producto
+			  </button>
+
+			  <input type="hidden" name="action" value="create_product">
+			
+			</form>
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+	        	Cancelar
+	        </button> 
+	      </div>
+	    </div>
+	  </div>
+	</div>
+
+	<div class="modal fade" id="updateModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	  <div class="modal-dialog">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <h1 class="modal-title fs-5" id="exampleModalLabel">
+	        	Editar producto
+	        </h1>
+	        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+	      </div>
+	      <div class="modal-body">
+	        <form method="POST" action="app/ProductController.php">
+			  
+			  <div class="mb-3">
+			    <label for="nombre" class="form-label">
+			    	Nombre
+			    </label>
+			    <input type="text" class="form-control" id="update_nombre" name="nombre" aria-describedby="emailHelp" required> 
+			  </div>
+			  
+			  <div class="mb-3">
+			    <label for="slug" class="form-label">
+			    	Slug
+			    </label>
+			    <input type="text" class="form-control" id="update_slug" name="slug" required> 
+			  </div>
+
+			  <div class="mb-3">
+			    <label for="slug" class="form-label">
+			    	description
+			    </label>
+			    <input type="text" class="form-control" id="update_description" name="description" required> 
+			  </div>
+
+			  <div class="mb-3">
+			    <label for="slug" class="form-label">
+			    	features
+			    </label>
+			    <input type="text" class="form-control" id="update_features" name="features" required> 
+			  </div>
+			  
+			  <div class="mb-3">
+			    <label for="slug" class="form-label">
+			    	Marca
+			    </label>
+			    
+			    <select class="form-control">
+			    	<?php if (isset($brands) && count($brands)): ?>
+			    	<?php foreach ($brands as $brand): ?>
+			    	<option value="<?= $brand->id ?>">
+			    		<?= $brand->name ?>
+			    	</option>
+			    	<?php endforeach ?>
+			    	<?php endif ?>
+			    	
+			    </select>
+
+			  </div>
+			  
+			  <button type="submit" class="btn btn-primary">
+			  	Crear producto
+			  </button>
+
+			  <input type="hidden" name="action" value="update_product">
+				
+			  <input type="hidden" name="product_id" id="product_id">
+
+			</form>
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+	        	Cancelar
+	        </button> 
+	      </div>
+	    </div>
+	  </div>
+	</div>
+
+	<form method="POST" id="remove_form" action="app/ProductController.php"> 
+
+	  <input type="hidden" name="action" value="delete_product">
+	  <input type="hidden" name="product_id" id="product_id_delete">
+
+	</form>
 	
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+	<script type="text/javascript">
+		
+		function editar(boton)
+		{
+
+			let producto = JSON.parse(boton.dataset.product);
+
+			console.log(producto.id)
+			
+			document.getElementById("update_nombre").value = producto.name
+			document.getElementById("update_slug").value = producto.slug
+			document.getElementById("update_description").value = producto.description
+			document.getElementById("update_features").value = producto.features
+			document.getElementById("product_id").value = producto.id
+			
+
+
+		}
+
+		function eliminar(product_id)
+		{
+			swal({
+			  title: "Are you sure?",
+			  text: "Once deleted, you will not be able to recover this imaginary file!",
+			  icon: "warning",
+			  buttons: true,
+			  dangerMode: true,
+			})
+			.then((willDelete) => {
+			  if (willDelete) {
+			    
+			    swal("Poof! Your imaginary file has been deleted!", {
+			      icon: "success",
+			    });
+
+			    document.getElementById("product_id_delete").value = product_id
+
+			    document.getElementById('remove_form').submit();
+			  
+			  } else {
+			    
+			  }
+			});
+		}
+
+	</script>
 </body>
 </html>
